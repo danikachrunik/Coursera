a7a5c2f7e08293174ad1034d6850f58705c2d07a
Danika Chrunik <danikachrunik@gmail.com>
2015-11-27 14:39:43 -0800

    rwd final assignment

--- responsiveWebDesign/Assignment2/assignment.js
+++ responsiveWebDesign/Assignment2/assignment.js
@@ -1,1 +1,19 @@
-// put your javascript code here
+$( document ).ready(function() {
+
+	$('#food_1').click(function() {
+		$('#food_1_img').show( "slow" );
+		$('#food_1').hide();
+		$('#reveal_text').hide();
+	});
+
+	$('#food_2').click(function() {
+		$('#food_2_img').show( "slow" );
+		$('#food_2').hide();
+	});
+
+	$('#food_3').click(function() {
+		$('#food_3_img').show( "slow" );
+		$('#food_3').hide();
+	});
+
+});
